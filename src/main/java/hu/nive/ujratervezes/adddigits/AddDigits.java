25b966c94f1afcfa9cc235f4d11e177f85a0e3e5
adamczego <cz4d4m@gmail.com>
2021-04-29 12:19:24 +0200

    complete task

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,25 @@
 package hu.nive.ujratervezes.adddigits;
 
+import java.util.Arrays;
+
+
+
 public class AddDigits {
-    //todo: implement your solution here
+
+    public static int addDigits(String input) {
+
+        if (input == null || input.equals("")) return -1;
+
+        String digitString = String.join("", input.replaceAll("\\D+", "").split(""));
+
+        if (digitString.equals("")) return 0;
+
+        return
+            Arrays.stream(digitString.split(""))
+            .map(Integer::parseInt)
+            .reduce(Integer::sum)
+            .orElse(0);
+
+    }
+
 }
